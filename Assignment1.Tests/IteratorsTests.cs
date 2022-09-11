28f411bde77768b2f33ef8c0072c3c7c76664878
Frederik Raisa <frederikblunck@gmail.com>
2022-09-11 08:37:19 +0200

    Refactor test

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -8,16 +8,13 @@ public class IteratorsTests
     public void Flatten_can_flatten()
     {
         // Arrange
-        var list1 = new List<int>() {1, 2, 3, 4, 5, 6};
-        var list2 = new List<int>() {7, 8, 9, 10, 11, 12};
-        var list3 = new List<int>() {13, 14, 15, 16, 17, 18};
-        var superList = new List<List<int>>(){list1, list2, list3};
+        var superList = new List<List<int>>{
+            new List<int>() { 1, 2, 3, 4, 5, 6 },
+            new List<int>() { 7, 8, 9, 10, 11, 12 },
+            new List<int>() { 13, 14, 15, 16, 17, 18 }
+        };
 
-        var expected = list1.Concat(list2).Concat(list3);
-
-        // int[,] array2D = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
-        // string[,] array2Db = new string[3, 2] { { "one", "two" }, { "three", "four" }, { "five", "six" } };
-        // IEnumerable.Cast<T>
+        var expected = superList[0].Concat(superList[1]).Concat(superList[2]);
 
         // Act
         var res = Iterators.Flatten<int>(superList);
@@ -27,12 +24,12 @@ public class IteratorsTests
     }
 
     [Fact]
-    public void Filter_can_filter_even_numbers() {
-        
+    public void Filter_can_filter_even_numbers()
+    {
         // arrange
-        var listOne = new List<int>() {1,2,3,4,5,6,7,8,9};
-        var expected = new List<int>() {2,4,6,8};
-        
+        var listOne = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        var expected = new List<int>() { 2, 4, 6, 8 };
+
         Predicate<int> even = Even;
         bool Even(int i) => i % 2 == 0;
 
@@ -44,12 +41,12 @@ public class IteratorsTests
     }
 
     [Fact]
-    public void Filter_can_filter_vowels() {
-        
+    public void Filter_can_filter_vowels()
+    {
         // arrange
-        var listOne = new List<char>() {'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E'};
-        var expected = new List<char>() {'a', 'A', 'e', 'E'};
-        
+        var listOne = new List<char>() { 'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E' };
+        var expected = new List<char>() { 'a', 'A', 'e', 'E' };
+
         Predicate<char> vowel = Vowel;
         bool Vowel(char i) => "aeiouæøå".Contains(Char.ToLower(i));
 
