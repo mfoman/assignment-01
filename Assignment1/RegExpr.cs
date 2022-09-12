cb57ce0da517dcf30f51e9b27d1e85556d1a505d
Amalie (amdh) <amdh@itu.dk>
2022-09-12 11:32:36 +0200

    Split line and Resolution done

--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,10 +1,31 @@
 namespace Assignment1;
+using System.Text.RegularExpressions;
 
 public static class RegExpr
 {
-    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) => throw new NotImplementedException();
+    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) 
+    {
+        var pattern = @"(\w+)";
+        foreach (var line in lines) 
+        {
+            foreach (Match match in Regex.Matches(line, pattern))
+            {
+                yield return match.Value;
+            }  
+        }
+    }
 
-    public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions) => throw new NotImplementedException();
+    public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions) 
+    {
+        var pattern = @"(?<width>\d+)x(?<height>\d+)";
+         foreach (var res in resolutions) 
+        {
+            foreach (Match match in Regex.Matches(res, pattern))
+            {
+                yield return (int.Parse(match.Groups["width"].Value), int.Parse(match.Groups["height"].Value));
+            }  
+        }
+    }
 
     public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
 
